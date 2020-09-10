1efe346f269fdb4db53c7c25ed8337155cb948f2
pedro <pedro.leandrog@gmail.com>
2020-09-10 15:46:16 -0300

    modelando uma tabela completa

--- behavior2/database/migrations/2020_09_10_180839_create_posts_table.php
+++ behavior2/database/migrations/2020_09_10_180839_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('subtitle');
+            $table->text('description');
+            $table->dateTime('publish_at');
             $table->timestamps();
         });
     }
